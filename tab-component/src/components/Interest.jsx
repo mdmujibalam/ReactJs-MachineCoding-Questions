5827f9176959f926d5ce1a5510547a043cc3475d
mdmujibalam <mujibalam2000@gmail.com>
2025-06-15 23:47:06 +0530

    Added Interest Filter logic using spread operator

--- tab-component/src/components/Interest.jsx
+++ tab-component/src/components/Interest.jsx
@@ -3,14 +3,28 @@ import "./Tabs.css";
 
 const Interest = ({ data, setData, errors }) => {
   const handleCheck = (e, item) => {
-    const newInterests = data.interests.filter((interest) => interest !== item);
+    // const newInterests = data.interests.filter((interest) => interest !== item);
 
-    if (e.target.checked) newInterests.push(item);
+    // if (e.target.checked) newInterests.push(item);
 
-    setData((prev) => ({
-      ...prev,
-      interests: newInterests,
-    }));
+    // setData((prev) => ({
+    //   ...prev,
+    //   interests: newInterests,
+    // })); 
+
+  setData((prev)=>{
+    if(e.target.checked){
+      //Add only if item if not present to avoid duplicancy
+     return  !prev.interests.includes(item)? {...prev,interests:[...prev.interests,item]} : prev
+    }
+    else{
+      //Remove item if unchecked
+      return {
+        ...prev,
+        interests: prev.interests.filter((interest)=> interest!=item)
+      }
+    }
+  })
   };
 
   return (
