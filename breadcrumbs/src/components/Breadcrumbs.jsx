bfb8335ea0d65f4ce91b177b359aaa915a5720fa
mdmujibalam <mujibalam2000@gmail.com>
2025-06-15 19:07:56 +0530

    Optimised performance by utilising useMemo in Breadcrumb component

--- breadcrumbs/src/components/Breadcrumbs.jsx
+++ breadcrumbs/src/components/Breadcrumbs.jsx
@@ -1,29 +1,41 @@
-import React from "react";
+import React,{useMemo} from "react";
 import { useLocation, Link } from "react-router-dom";
 
 const Breadcrumbs = () => {
   const { pathname } = useLocation();
   //console.log(pathname);
+
+
+const breadcrumbs=useMemo(()=>{
   const pathsArr = pathname.split("/").filter((x) => x);
-  //console.log(pathsArr);
-  let breadcrumbPath = "";
+  return pathsArr.map((name, index) => {
+    return {
+      name,
+      path: "/" + pathsArr.slice(0, index + 1).join("/"),
+    };
+  })
+},[pathname]);
+
+//console.log("breadcrubms", breadcrumbs);
 
   return (
     <>
       <Link to="/">Home</Link>
-      {pathsArr.map((name, index) => {
-        breadcrumbPath += `/${name}`;
-        const isLast = index === pathsArr.length - 1;
+      
+      {breadcrumbs.map((crumb, index) => {
+        const isLast = index === breadcrumbs.length - 1;
 
-        return isLast ? (
-          <span key={index}>/{name}</span>
-        ) : (
-          <span key={index}>
-            /<Link to={breadcrumbPath}>{name}</Link>
+        return (
+          <span key={crumb.path}>
+            {"/"}
+
+            {isLast ? (
+              <span key={crumb.path}>{crumb.name}</span>
+            ) : (
+              <Link to={crumb.path}>{crumb.name}</Link>
+            )}
           </span>
         );
-
-        //return <span key={index}>/{name}</span>
       })}
     </>
   );
