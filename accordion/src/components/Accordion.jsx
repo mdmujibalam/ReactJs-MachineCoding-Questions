e39a17b19af90ac0716d01639368672e14559bb2
mdmujibalam <mujibalam2000@gmail.com>
2025-06-18 23:59:54 +0530

    Added multiple accordion open functionality

--- accordion/src/components/Accordion.jsx
+++ accordion/src/components/Accordion.jsx
@@ -3,25 +3,34 @@ import "./Accordion.css";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 
 const Accordion = ({ items }) => {
-  const [activeIndex, setActiveIndex] = useState(null);
+  //const [activeIndex, setActiveIndex] = useState(null);
+  const [openIndexes,setOpenIndexes] = useState(new Array(items.length).fill(false));
 
-  const handleClick = (index) => {
-    setActiveIndex(activeIndex === index ? null : index);
-  };
+  // const handleClick = (index) => {
+  //   setActiveIndex(activeIndex === index ? null : index);
+  // };
+
+  const handleIndexes=(index)=>{
+    setOpenIndexes((prev)=>{
+      const newOpenIndexs=[...prev];
+      newOpenIndexs[index]= !newOpenIndexs[index];
+      return newOpenIndexs;
+    })
+  }
 
   return (
     <div>
       {items.map((item, index) => (
         <div className="accordion-container" key={index}>
-          <div className="accordion-header" onClick={() => handleClick(index)}>
+          <div className="accordion-header" onClick={() => handleIndexes(index)}>
             <div className="accordion-title">{item.title}</div>
 
             <div className="accordion-icon">
-              {activeIndex === index ? <FaChevronUp /> : <FaChevronDown />}
+              {openIndexes[index] ? <FaChevronUp /> : <FaChevronDown />}
             </div>
           </div>
 
-          {activeIndex === index && (
+          {openIndexes[index] && (
             <div className="accordion-body">{item.content}</div>
           )}
         </div>
